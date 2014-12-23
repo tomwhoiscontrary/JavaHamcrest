f2febd491945d80a4ccd60a0685564d90161ccc5
Nat Pryce <sw@natpryce.com>
2014-12-23 16:44:26 +0000

    Generalised HasSubsequenceMatcher to be a matcher of Iterable, not Collection

--- hamcrest-library/src/main/java/org/hamcrest/collection/HasSubsequence.java
+++ hamcrest-library/src/main/java/org/hamcrest/collection/HasSubsequence.java
@@ -1,22 +1,17 @@
 package org.hamcrest.collection;
 
-import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.hamcrest.Description;
-import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
-import static org.hamcrest.core.IsEqual.equalTo;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Collection;
 
 /**
  * Matches if a collection contains another collection's items in
  * consecutive order anywhere inside it.
  */
-public class HasSubsequence<T>
-    extends TypeSafeDiagnosingMatcher<Collection<? extends T>> {
+public class HasSubsequence<T> extends TypeSafeDiagnosingMatcher<Iterable<? extends T>> {
     private final List<Matcher<? super T>> matchers;
 
     public HasSubsequence(List<Matcher<? super T>> matchers) {
@@ -24,10 +19,13 @@ public class HasSubsequence<T>
     }
 
     @Override
-    public boolean matchesSafely(
-            Collection<? extends T> subsequenceToMatch,
-            Description mismatchDescription) {
-        List<? extends T> subsequenceToMatchList = new ArrayList<T>(subsequenceToMatch);
+    public boolean matchesSafely(Iterable<? extends T> subsequenceToMatch,
+                                 Description mismatchDescription)
+    {
+        List<T> subsequenceToMatchList = new ArrayList<>();
+        for (T e : subsequenceToMatch) {
+            subsequenceToMatchList.add(e);
+        }
 
         for (int i = 0; i <= subsequenceToMatchList.size() - matchers.size(); i++) {
             boolean allMatchersMatched = true;
@@ -54,32 +52,5 @@ public class HasSubsequence<T>
         description
                 .appendText("collection contains subsequence matching ")
                 .appendList("[", ", ", "]", matchers);
-    }
-
-    @Factory
-    public static <T> Matcher<Collection<? extends T>> hasSubsequence(
-            List<Matcher<? super T>> matchers) {
-        return new HasSubsequence<T>(matchers);
-    }
-
-    @Factory
-    public static <T> Matcher<Collection<? extends T>> hasSubsequence(
-            Matcher<? super T>... matchers) {
-        return hasSubsequence(Arrays.asList(matchers));
-    }
-
-    @Factory
-    public static <T> Matcher<Collection<? extends T>> hasSubsequence(
-            Iterable<? extends T> items) {
-        List<Matcher<? super T>> matchers = new ArrayList<Matcher<? super T>>();
-        for (Object item : items) {
-            matchers.add(equalTo(item));
-        }
-        return new HasSubsequence<T>(matchers);
-    }
-
-    @Factory
-    public static <T> Matcher<Collection<? extends T>> hasSubsequence(T... elements) {
-        return hasSubsequence(Arrays.asList(elements));
     }
 }
