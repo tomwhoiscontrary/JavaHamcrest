f54712c12e02778c0ace1d425084f25953bf45f3
sf105 <github@m3p.co.uk>
2014-12-26 11:53:28 +0100

    Constant values for File matchers

--- hamcrest-library/src/main/java/org/hamcrest/io/FileMatchers.java
+++ hamcrest-library/src/main/java/org/hamcrest/io/FileMatchers.java
@@ -13,25 +13,19 @@ import static org.hamcrest.core.IsEqual.equalTo;
 
 public final class FileMatchers {
 
-    public static Matcher<File> anExistingDirectory() {
-        return fileChecker(IS_DIRECTORY, "an existing directory", "is not a directory");
-    }
+    public static Matcher<File> anExistingDirectory() { return AN_EXISTING_DIRECTORY; }
 
-    public static Matcher<File> anExistingFileOrDirectory() {
-        return fileChecker(EXISTS, "an existing file or directory", "does not exist");
-    }
+    public static Matcher<File> anExistingFileOrDirectory() { return EXISTING_FILE_OR_DIRECTORY; }
 
     public static Matcher<File> anExistingFile() {
-        return fileChecker(IS_FILE, "an existing File", "is not a file");
+        return EXISTING_FILE;
     }
 
     public static Matcher<File> aReadableFile() {
-        return fileChecker(CAN_READ, "a readable File", "cannot be read");
+        return READABLE_FILE;
     }
 
-    public static Matcher<File> aWritableFile() {
-        return fileChecker(CAN_WRITE, "a writable File", "cannot be written to");
-    }
+    public static Matcher<File> aWritableFile() { return WRITEABLE_FILE; }
 
     public static Matcher<File> aFileWithSize(long size) {
         return aFileWithSize(equalTo(size));
@@ -49,38 +43,32 @@ public final class FileMatchers {
         return new FunctionMatcher<>("A file with canonical path", "path", expected, CANONICAL_PATH);
     }
 
-
     public static Matcher<File> aFileWithAbsolutePath(final Matcher<String> expected) {
         return new FunctionMatcher<>("A file with absolute path", "path", expected, ABSOLUTE_PATH);
     }
 
-    public static interface FileStatus {
-        boolean check(File actual);
-    }
+    private static final Matcher<File> AN_EXISTING_DIRECTORY = fileChecker("an existing directory", "is not a directory",
+        new FileStatus() { @Override public Boolean from(File actual) { return actual.isDirectory(); } });
 
-    public static final FileStatus CAN_WRITE = new FileStatus() {
-        @Override public boolean check(File actual) { return actual.canWrite(); }
-    };
-    public static final FileStatus CAN_READ = new FileStatus() {
-        @Override public boolean check(File actual) { return actual.canRead(); }
-    };
+    private static final Matcher<File> EXISTING_FILE_OR_DIRECTORY = fileChecker("an existing file or directory", "does not exist",
+        new FileStatus() { @Override public Boolean from(File actual) { return actual.exists(); } });
 
-    public static final FileStatus IS_FILE = new FileStatus() {
-        @Override public boolean check(File actual) { return actual.isFile(); }
-    };
+    public static final Matcher<File> EXISTING_FILE = fileChecker("an existing File", "is not a file",
+        new FileStatus() { @Override public Boolean from(File actual) { return actual.isFile(); } });
 
-    public static final FileStatus IS_DIRECTORY = new FileStatus() {
-        @Override public boolean check(File actual) { return actual.isDirectory(); }
-    };
+    public static final Matcher<File> READABLE_FILE = fileChecker("a readable File", "cannot be read",
+        new FileStatus() { @Override public Boolean from(File actual) { return actual.canRead(); } });
 
-    public static final FileStatus EXISTS = new FileStatus() {
-        @Override public boolean check(File actual) { return actual.exists(); }
-    };
+    public static final Matcher<File> WRITEABLE_FILE = fileChecker("a writable File", "cannot be written to",
+        new FileStatus() { @Override public Boolean from(File actual) { return actual.canWrite(); } });
 
-    private static Matcher<File> fileChecker(final FileStatus fileStatus, final String successDescription, final String failureDescription) {
+
+    public static interface FileStatus extends Feature<File, Boolean> { }
+
+    public static Matcher<File> fileChecker(final String successDescription, final String failureDescription, final FileStatus fileStatus) {
         return new TypeSafeDiagnosingMatcher<File>() {
             public boolean matchesSafely(File actual, Description mismatchDescription) {
-                final boolean result = fileStatus.check(actual);
+                final boolean result = fileStatus.from(actual);
                 if (!result) {
                     mismatchDescription.appendText(failureDescription);
                 }
