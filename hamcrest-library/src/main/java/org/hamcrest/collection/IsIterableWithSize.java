5c09dfa9f3a129aa123691aac00cdf4a9fc24573
Nat Pryce <sw@natpryce.com>
2014-12-28 19:17:38 +0000

    Tiny refactor to address a niggly bit of code

--- hamcrest-library/src/main/java/org/hamcrest/collection/IsIterableWithSize.java
+++ hamcrest-library/src/main/java/org/hamcrest/collection/IsIterableWithSize.java
@@ -3,22 +3,18 @@ package org.hamcrest.collection;
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 
-import java.util.Iterator;
-
 public class IsIterableWithSize<E> extends FeatureMatcher<Iterable<E>, Integer> {
-
     public IsIterableWithSize(Matcher<? super Integer> sizeMatcher) {
         super(sizeMatcher, "an iterable with size", "iterable size");
     }
-    
+
 
     @Override
     protected Integer featureValueOf(Iterable<E> actual) {
-      int size = 0;
-      for (Iterator<E> iterator = actual.iterator(); iterator.hasNext(); iterator.next()) {
-        size++;
-      }
-      return size;
+        int size = 0;
+        for (E e : actual) {
+            size++;
+        }
+        return size;
     }
-
 }
